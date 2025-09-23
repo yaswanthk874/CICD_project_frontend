f39247e5d7cfa8a4e16b0b374a904e7cbdc5c76c
MSVVSSAINADH <macharlasainadh2506@gmail.com>
2025-09-23 17:51:42 +0530

    v1.0.2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CICD_HOSPITAL_MANEGEMENT_SYSTEM",
+  base: "/hms",
 });
 
