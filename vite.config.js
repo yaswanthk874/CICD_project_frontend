54b28f929ec07486a2d0bd90a466d7619dbab476
MSVVSSAINADH <macharlasainadh2506@gmail.com>
2025-11-02 12:13:36 +0530

    V1.3.2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CRITICAL FIX: Ensure assets are resolved relative to the base URL
-  base: './', 
+  // CRITICAL FIX: Explicitly set the base path to the root.
+  base: '/', 
 })
